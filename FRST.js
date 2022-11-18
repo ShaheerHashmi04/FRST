3adda1fd43e6912828ff890b14d2f2aeee85353a
Ranveer2323 <116836777+Ranveer2323@users.noreply.github.com>
2022-11-18 13:14:29 -0500

    Add files via upload

---
+++ FRST.js
@@ -0,0 +1,15 @@
+function b1() {
+
+    document.getElementById("stuff").style.background = "red";
+    
+}
+function b2() {
+
+    document.getElementById("stuff").style.background = "blue"
+
+}
+function b3() {
+
+    document.getElementById("stuff").style.background = "green"
+
+}
